245da791df9845fb229458e737f48a4c000ba1e5
pwnyboi <112465699+pwnyboi@users.noreply.github.com>
2022-11-30 11:48:51 -0700

    updated

--- ytboi.py
+++ ytboi.py
@@ -2,12 +2,27 @@ from pytube import YouTube
 from sys import argv
 
 link = input("Please enter YouTube link to download: ")
+dest = input("Please enter destination to download? Leave blank if no destination")
 #link = argv[1]
 #link = "https://www.youtube.com/watch?v=vEQ8CXFWLZU"
 yt = YouTube(link)
 
-print("Title: ", yt.title)
-print("Views: ", yt.views)
-yd = yt.streams.get_highest_resolution()
-yd.download(r"C:/Users/Black/Documents/GitHub/ytboi/Downloads")
-print("Download Successful!")
+#yd = yt.streams.get_highest_resolution()
+#yd.download(r"C:/Users/Black/Documents/GitHub/ytboi/Downloads")
+#print("Download Successful!")
+
+
+def downloader():
+    yd = yt.streams.get_highest_resolution()
+    yd.download(dest)
+    #yd.download(r"C:/Users/Black/Documents/GitHub/ytboi/Downloads")
+    print("Downloading: ", yt.title)
+    print("This video has: ", yt.views)
+    print("Download Successful!")
+    
+def ask_continue():
+
+
+
+downloader()
+ask_continue()
