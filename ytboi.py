b12f6f5c37d412cdbf940d95e082bdefca568c20
pwnyboi <112465699+pwnyboi@users.noreply.github.com>
2022-11-30 11:13:44 -0700

    Update ytboi.py

--- ytboi.py
+++ ytboi.py
@@ -1,12 +1,12 @@
 from pytube import YouTube 
 from sys import argv
 
+link = input("Please enter YouTube link to download: ")
 #link = argv[1]
-link = "https://www.youtube.com/watch?v=vEQ8CXFWLZU"
+#link = "https://www.youtube.com/watch?v=vEQ8CXFWLZU"
 yt = YouTube(link)
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
 yd = yt.streams.get_highest_resolution()
-yd.download(r"C:/Users/Black/Documents/GitHub/ytboi/")
-#yd.download(r"C:/Users/Blac/Desktop/YTBOI_DL/")
+yd.download(r"C:/Users/Black/Documents/GitHub/ytboi/Downloads")
