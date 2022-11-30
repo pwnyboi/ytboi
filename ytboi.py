7e049e5af41c2637f7865e5a90f94f30ba517a2e
pwnyboi <112465699+pwnyboi@users.noreply.github.com>
2022-11-30 12:17:48 -0700

    update

--- ytboi.py
+++ ytboi.py
@@ -7,16 +7,17 @@ dest = input("Please enter destination to download? Leave blank if no destinatio
 
 def downloader():
     link = input("Please enter YouTube link to download: ")
+    print("Downloading: ", yt.title)
     yt = YouTube(link)
     yd = yt.streams.get_highest_resolution()
     yd.download(dest)
-    print("Downloading: ", yt.title)
+
     print("This video has: ", yt.views)
     print("Download Successful!")
     time.sleep(.69)
 
 def ask_continue():
-    continue_downloading = input("Want to download another video? y/n")
+    continue_downloading = input("Want to download another video? y/n: ")
     if continue_downloading == "Y":
         return
     if continue_downloading == "y":
